fee5ac66b62fe8f048b10eb2bf1eb56df57108dd
shivom <singhshivom230@gmail.com>
2020-12-10 00:15:03 +0530

    added api in app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+var inputArea = document.querySelector("textarea");
 var txtButton = document.querySelector(".txt-button");
+var outputArea = document.querySelector(".output-area");
+
+var serverUrl = "https://api.funtranslations.com/translate/article_rewrite.json"
+
+function clickHandler(){
+    var url = serverUrl + "?text=" + inputArea.value;
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        var outputTxt = json.contents.translated
+
+        outputArea.innerText = outputTxt
+    })
+
+}
+
+txtButton.addEventListener("click", clickHandler)
